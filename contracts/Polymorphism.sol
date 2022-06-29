9babe3d34fe4ba7641a2e4e609552bf26e56d0e1
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-06-29 18:56:58 +0530

    Update Polymorphism.sol

--- contracts/Polymorphism.sol
+++ contracts/Polymorphism.sol
@@ -3,15 +3,5 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Polymorphism {
-    function sub(int256 a, int256 b) public pure returns (int256) {
-        return a - b;
-    }
-
-    function sub(
-        int256 a,
-        int256 b,
-        int256 c
-    ) public pure returns (int256) {
-        return a - b - c;
-    }
+    
 }
